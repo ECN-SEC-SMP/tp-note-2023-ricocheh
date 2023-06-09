9a4ab15ad1c192f5889ca15ec279a3c948a40f44
Alexis <alexis.lequeux1003@outlook.fr>
2023-06-09 13:39:56 +0200

    fixup! Ajout d'une classe de gestion du sablier

--- Sablier.cpp
+++ Sablier.cpp
@@ -1,8 +1,9 @@
 /**
+ * @file Robot.cpp
+ * @brief Implémentation de la classe Robot
  * ASSE Romain FERRE Cedric, PROCACCIA Paul, LEQUEUX Alexis
  * tp ricochet robots
- *
- * Partie gestion du temps - Sablier
+ * @author Alexis LEQUEUX <alexis.lequeux@eleves.ec-nantes.fr>
  */
 
 /******* Native include *******/
@@ -18,64 +19,77 @@ using namespace std;
 #include <chrono>
 #include <thread>
 
-//************METHODE************
-/*
-Le Sablier est implemente avec la methode suivante. Nous utilisons un chrono que l'on lance avec la fonction start,
-que l'on arrete avec la focntion stop et que l'on remet a 0 avec la fonciton reset
 
-Lorsque l'on utilise la fonction isActif cette fonction va nous dire si il est actif (Sablier en cours)
-Ou bien si il n'est pas actif qui se traduit par deux situations possibles:
--Le Sablier n'a pas ete lance 
--Le temps imparti est ecoule
-*/
-
-
-//************Constructeur************
-Sablier::Sablier(int temps){
-this->secondes=temps*1000;// enregistrement du temps en ms
-this->tempsEcoule=0;//mise a 0 du temps ecoule
+/******* Constructeur *******/
+/**
+ * @brief Initialisation du sablier
+ * @param temps temps du sablier en seconde
+ */
+Sablier::Sablier(int temps)
+{
+    this->secondes = temps * 1000;
+    this->tempsEcoule = 0;
 }
-//************STOP************
-void Sablier::stop(void){
-if (actif) {
-            chrono::steady_clock::time_point tempsFin =chrono::steady_clock::now();//memorisation du temps de fin
-            tempsEcoule += chrono::duration_cast<chrono::milliseconds>(tempsFin - tempsDepart).count();//calcul du temps
-            actif = false;//desactivation du Sablier
-            cout << "Temps: " << tempsEcoule <<endl;// affichage du temps
-        }
-}
-
-//************START************
-void Sablier::start(void){
-    if (!actif) // si le Sablier n'est pas lancé
+/******* STOP *******/
+/**
+ * @brief Stop le sablier
+ */
+void Sablier::stop(void)
+{
+    if (actif) 
     {
-        actif = true;//passage à l'état actif
-        tempsDepart = chrono::steady_clock::now();//lancement du chrono et memorisation du temps de départ
-    } else 
-    {
-                cout << "Deja lance" << endl;//message d'erreur si le Sablier est actif
+        chrono::steady_clock::time_point tempsFin = chrono::steady_clock::now();
+        tempsEcoule += chrono::duration_cast<chrono::milliseconds>(tempsFin - tempsDepart).count();
+        cout << "Temps: " << tempsEcoule <<endl;
     }
 }
-//************RESET************
+
+/******* START *******/
+/**
+ * @brief Lance le sablier
+ */
+void Sablier::start(void)
+{
+    if (!actif)
+    {
+        actif = true;
+        tempsDepart = chrono::steady_clock::now();
+    } 
+    else 
+    {
+        cout << "Deja lance" << endl;
+    }
+}
+/******* RESET *******/
+/**
+ * @brief Remet à 0 le temps écoulé
+ */
 void Sablier::reset(void){
-    actif = false;// descativation du Sablier
-    tempsEcoule=0;//mise a 0 du temps ecoule
+    actif = false;
+    tempsEcoule = 0;
 }
 
-//************ETAT DU SABLIER************
+/******* isActive *******/
+/**
+ * @brief Donne l'état du sablier
+ * @return vrai ou faux => ON / OFF
+ */
 bool Sablier::isActive(void){
-    chrono::steady_clock::time_point tempsFinVerif =chrono::steady_clock::now();//calcul du temps restant du Sablier
-    long int tempsActuel = chrono::duration_cast<chrono::milliseconds>(tempsFinVerif - tempsDepart).count();//calcul du temps restant du Sablier
-    if(tempsActuel>secondes){// si le temps depasse la valeur du Sablier => Sablier termine
-        this->stop(); //on arrete
-        this->reset(); //on remet a 0
-        return false; // retourne faux (pas de Sablier actif, car il est fini)
+    chrono::steady_clock::time_point tempsFinVerif = chrono::steady_clock::now();
+    long int tempsActuel = chrono::duration_cast<chrono::milliseconds>(tempsFinVerif - tempsDepart).count();
+    if(tempsActuel > secondes)
+    {
+        this->stop();
+        this->reset();
+        return false; 
     }
-    else if(actif==false){// si le Sablier n'est pas actif (stop ou reset)
-        return false; // retourne faux
+    else if(actif == false)
+    {
+        return false;
     }
-    else{
-        return true; // sinon il est actif, on retourne actif
+    else
+    {
+        return true;
     }
 
 }
--- Sablier.hpp
+++ Sablier.hpp
@@ -1,16 +1,31 @@
+/**
+ * @file Sablier.hpp
+ * @brief La déclaration de la classe Sablier
+ * ASSE Romain FERRE Cedric, PROCACCIA Paul, LEQUEUX Alexis
+ * tp ricochet robots
+ * @author Alexis LEQUEUX <alexis.lequeux@eleves.ec-nantes.fr>
+ */
+
+/******* Native include *******/
 #include <iostream>
+/******* Espace de nommage ****/
 using namespace std;
 
-class Sablier {
-private:
-    int secondes;
-    int tempsEcoule;
-    chrono::steady_clock::time_point tempsDepart;
-    bool actif;
-public:
-    Sablier(int temps);
-    void stop(void);
-    void start(void);
-    void reset(void);
-    bool isActive(void);
+/**
+ * @brief Déclaration de la classe Sablier
+ */
+class Sablier 
+{
+    private:
+        int secondes;
+        int tempsEcoule;
+        chrono::steady_clock::time_point tempsDepart;
+        bool actif;
+
+    public:
+        Sablier(int temps);
+        void stop(void);
+        void start(void);
+        void reset(void);
+        bool isActive(void);
 };
