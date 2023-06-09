648df7f69d5a7ac739fee5e053101b1c8ed7cc1b
Alexis <alexis.lequeux1003@outlook.fr>
2023-06-09 13:35:05 +0200

    fixup! Ajout d'une classe de gestion des robots

--- Robot.cpp
+++ Robot.cpp
@@ -1,8 +1,9 @@
 /**
+ * @file Robot.cpp
+ * @brief Implémentation de la classe Robot
  * ASSE Romain FERRE Cedric, PROCACCIA Paul, LEQUEUX Alexis
  * tp ricochet robots
- *
- * Partie Robot
+ * @author Alexis LEQUEUX <alexis.lequeux@eleves.ec-nantes.fr>
  */
 
 /******* Native include *******/
@@ -10,65 +11,271 @@
 
 /******* Project include ******/
 #include "Robot.hpp"
-
+#include "Cell.hpp"
+#include "Board.hpp"
 /******* Espace de nommage ****/
 using namespace std;
 
 //************Constructeur************
-Robot::Robot(int x,int y, couleur couleurrobot){
-    this->positionX=x;
-    this->positionY=y;
-    this->color=couleurrobot;
+/**
+ * @brief Positionne le robot, enregistre la couleur, occupe une case du plateau de jeu
+ * @param x choix de la colonne du jeu
+ * @param y choix de la ligne du jeu
+ * @param couleur choix de la couleur du robot
+ * @param gameBoard passage de l'adresse du plateau de jeu
+ */
+Robot::Robot(int x, int y, couleur couleurrobot, Board *b1)
+{
+    this->positionX = x;
+    this->positionY = y;
+    this->color = couleurrobot;
+    Cell* updated; 
+    updated = b1->getCell(y, x);
+    updated->setTaken(true);
 }
 
-int Robot::getX(void){
+/**
+ * @brief permet de recuperer la colonne du robot
+ * @return colonne du robot
+ */
+int Robot::getX(void)
+{
     return this->positionX;
 }
-int Robot::getY(void){
-    return this->positionY;
-}
-void Robot::setX(int x){ //ajouter un test pour savoir si on peut ajouter le pion à cette case
-    this->positionX=x; // compliquer du fait de la séparation du X et du Y
-}
-void Robot::setY(int y){//ajouter un test pour savoir si on peut ajouter le pion à cette case
-    this->positionY=y;// compliquer du fait de la séparation du X et du Y
-}
-void Robot::deplacement(enum sens){
-/*
-if (sens==1){//gauche
-    int a = this->getX;
 
-while(a>0 && !a.isWall){
-    a--;
-}
-this->setX(a);
+/**
+ * @brief permet de recuperer la ligne du robot
+ * @return ligne du robot
+ */
+int Robot::getY(void)
+{
+    return this -> positionY;
 }
 
-if (sens==2){//droite
-    int a = this->getX;
-
-while(a<MAXligne && !a.isWall){
-    a++;
+/**
+ * @brief permet de mettre le robot à une colonne précise du jeu. MàJ du statut occuppé.
+ * @param x choix de la colonne du jeu
+ * @param b adresse de la board du jeu
+ */
+void Robot::setX(int x, Board *b)
+{ 
+    int oldX = this->getX();
+    int oldY = this->getY();
+    Cell* initial; 
+    Cell* updated; 
+    initial = b->getCell(oldY,oldX);
+    updated = b->getCell(oldY,x);
+    initial->setTaken(false);
+    updated->setTaken(true);
+    this->positionX = x;
 }
-this->setX(a);
+
+/**
+ * @brief permet de mettre le robot à une ligne précise du jeu. MàJ du statut occuppé.
+ * @param y choix de la ligne du jeu
+ * @param b adresse de la board du jeu
+ */
+void Robot::setY(int y,Board *b)
+{
+    int oldX = this->getX();
+    int oldY = this->getY();
+    Cell* initial; 
+    Cell* updated; 
+    initial = b->getCell(oldY,oldX);
+    updated = b->getCell(y,oldX);
+    initial->setTaken(false);
+    updated->setTaken(true);
+    this->positionY= y ;
 }
 
-if (sens==3){//haut
-    int a = this->getY;
+/**
+ * @brief permet de deplacer le robot dans une des 4 directions. MàJ du statut occuppé.
+ * @param direction left right up down
+ * @param b1 adresse de la board du jeux 
+ */
+void Robot::deplacement(sens direction, Board *b1)
+{
+    if (direction==sens::left)
+    {
+        int x = this->getX();
+        int y = this->getY();
+        Cell* initial; 
+        Cell* next; 
+        initial = b1->getCell(y,x); 
+        if (!initial->isWalled(Cell::WALL_LEFT))
+        {
+            x--;
+            while(x>0)
+            {
+                next = b1->getCell(y,x);
+                if(!next->isTaken())
+                {
+                    if(!next->isWalled(Cell::WALL_RIGHT))
+                    {
+                        if((!next->isWalled(Cell::WALL_LEFT)))
+                        {
+                            x--;
+                        }
+                        else
+                        {
+                            this->setX(x , b1);
+                            return; 
+                        }
+                    }
+                    else
+                    {
+                        this->setX(x + 1 , b1);
+                        return;
+                    }
+                }
+                else
+                {
+                    this->setX(x + 1 , b1);
+                    return;
+                } 
+            }
+            this->setX(0 , b1);
+            return;
+        }
+        this->setX(x , b1);
+        return;
+    }
 
-while(a<MAXligne && !a.isWall){
-    a++;
-}
-this->setY(a);
-}
+    if (direction==sens::right)
+    {
+        int x = this->getX();
+        int y = this->getY();
+        Cell* initial; 
+        Cell* next; 
+        initial = b1->getCell(y,x); 
+        if (!initial->isWalled(Cell::WALL_RIGHT))
+        {
+            x++;
+            while(x<16)
+            {
+                next = b1->getCell(y,x);
+                if(!next->isTaken())
+                {
+                    if(!next->isWalled(Cell::WALL_LEFT))
+                    {
+                        if((!next->isWalled(Cell::WALL_RIGHT)))
+                        {
+                            x++;
+                        }
+                        else
+                        {
+                            this->setX(x , b1);
+                            return; 
+                        }
+                    }
+                    else
+                    {
+                        this->setX(x - 1 , b1);
+                        return;
+                    }
+                }
+                else
+                {
+                    this->setX(x - 1 , b1);
+                    return;
+                } 
+            }
+            this->setX(15 , b1);
+            return;
+        }
+        this->setX(x , b1);
+        return;
+    }
 
-if (sens==4){//bas
-    int a = this->getY;
+    if (direction==sens::up)
+    {
+        int x = this->getX();
+        int y = this->getY();
+        Cell* initial; 
+        Cell* next; 
+        initial = b1->getCell(y,x); 
+        if (!initial->isWalled(Cell::WALL_UP))
+        {
+            y++;
+            while(y<16)
+            {
+                next = b1->getCell(y,x);
+                if(!next->isTaken())
+                {
+                    if(!next->isWalled(Cell::WALL_DOWN))
+                    {
+                        if((!next->isWalled(Cell::WALL_UP)))
+                        {
+                            y++;
+                        }
+                        else
+                        {
+                            this->setY(y , b1);
+                            return; 
+                        }
+                    }
+                    else
+                    {
+                        this->setY(y - 1 , b1);
+                        return;
+                    }
+                }
+                else
+                {
+                    this->setY(y - 1 , b1);
+                    return;
+                } 
+            }
+            this->setY(15 , b1);
+            return;
+        }
+        this->setY(y , b1);
+        return;
+    }
 
-while(a>0 && !a.isWall){
-    a--;
-}
-this->setY(a);
-
-*/ 
+    if (direction==sens::down)
+    {
+        int x = this->getX();
+        int y = this->getY();
+        Cell* initial; 
+        Cell* next; 
+        initial = b1->getCell(y,x); 
+        if (!initial->isWalled(Cell::WALL_DOWN))
+        {
+            y--;
+            while(y>0)
+            {
+                next = b1->getCell(y,x);
+                if(!next->isTaken())
+                {
+                    if(!next->isWalled(Cell::WALL_UP))
+                    {
+                        if((!next->isWalled(Cell::WALL_DOWN)))
+                        {
+                            y--;
+                        }
+                        else
+                        {
+                            this->setY(y , b1);
+                            return; 
+                        }
+                    }
+                    else
+                    {
+                        this->setY(y + 1 , b1);
+                        return;
+                    }
+                }
+                else
+                {
+                    this->setY(y + 1 , b1);
+                    return;
+                } 
+            }
+            this->setY(0 , b1);
+            return;
+        }
+        this->setY(y , b1);
+        return;
+    }
 }
--- Robot.hpp
+++ Robot.hpp
@@ -1,18 +1,33 @@
 /**
+ * @file Robot.hpp
+ * @brief La déclaration de la classe Robot
  * ASSE Romain FERRE Cedric, PROCACCIA Paul, LEQUEUX Alexis
  * tp ricochet robots
- *
- * Partie robot
+ * @author Alexis LEQUEUX <alexis.lequeux@eleves.ec-nantes.fr>
  */
 
 /******* Native include *******/
 #include <iostream>
+#include "Board.hpp"
 
 /******* Espace de nommage ****/
 using namespace std;
-enum couleur{jaune,vert,rouge, bleu};
-enum sens{left,right, up, down};
 
+/**
+ * @enum Couleur
+ * @brief Couleur possible que le robot peut avoir
+ */
+enum couleur{jaune, vert, rouge, bleu};
+
+/**
+ * @enum Sens
+ * @brief Différent sens dans lequel on peut déplacer le robot
+ */
+enum sens{left, right, up, down};
+
+/**
+ * @brief Déclaration de la classe Robot
+ */
 class Robot
 {
     private:
@@ -21,11 +36,10 @@ class Robot
         couleur color;
 
     public:
-
-Robot(int x,int y, couleur couleurrobot);
-    void deplacement(sens);
-    int getX(void);
-    int getY(void);
-    void setX(int x);
-    void setY(int y);
+        Robot(int x,int y, couleur couleurrobot, Board *b1);
+        void deplacement(sens direction, Board *b1);
+        int getX(void);
+        int getY(void);
+        void setX(int x, Board *b);
+        void setY(int y, Board *b);
 };
